c7650609b7efa779c650055daac38c38f54b1815
Bryan Freitas <bryan.m.freitas@gmail.com>
2020-10-02 21:28:59 +0000

    Increased Marker Dot Size

--- src/calendar/day/multi-dot/style.js
+++ src/calendar/day/multi-dot/style.js
@@ -39,8 +39,8 @@ export default function styleConstructor(theme={}) {
       color: appStyle.textDisabledColor
     },
     dot: {
-      width: 4,
-      height: 4,
+      width: 10,
+      height: 10,
       marginTop: 1,
       marginLeft: 1,
       marginRight: 1,
